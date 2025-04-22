4fa8e0d74569d425fedd46658c3f5be32b774ea1
Margarita Sosnovskaya <m.sosnovskaya@qiwi.com>
2025-04-22 23:11:52 +0300

    Add dispatcher servlet.

---
+++ yandex-practicum-blog-app/src/main/java/edu/misosnovskaya/WebConfiguration.java
@@ -0,0 +1,11 @@
+package edu.misosnovskaya;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "edu.misosnovskaya.controller")
+public class WebConfiguration {
+}
--- yandex-practicum-blog-app/src/main/webapp/index.jsp
+++ yandex-practicum-blog-app/src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2><%= "Hello World!" %></h2>
+<h2><%= "Let's go!" %></h2>
 </body>
 </html>
