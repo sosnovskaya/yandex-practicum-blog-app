f8e170a3c29e646dd7a46d2331a7864a0999000b
Margarita Sosnovskaya <m.sosnovskaya@qiwi.com>
2025-04-24 20:55:09 +0300

    Change web configuration.

--- yandex-practicum-blog-app/src/main/java/edu/misosnovskaya/configuration/WebConfiguration.java
+++ yandex-practicum-blog-app/src/main/java/edu/misosnovskaya/configuration/WebConfiguration.java
@@ -1,4 +1,4 @@
-package edu.misosnovskaya;
+package edu.misosnovskaya.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
--- yandex-practicum-blog-app/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2><%= "Let's go!" %></h2>
-</body>
-</html>
